ae28323bafdda2666a81b271b936c8d64407ec75
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-10-18 15:17:02 +0200

    feat: chapter 15 authentication

---
+++ auth.ts
@@ -0,0 +1,50 @@
+import NextAuth, { CredentialsSignin } from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+async function getUser(email: string): Promise<User | undefined> {
+	try {
+		const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+		return user[0];
+	} catch {
+		throw new Error('Invalid email or password');
+	}
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+	...authConfig,
+	providers: [
+		Credentials({
+			credentials: {
+				username: { label: 'Username' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				const parsedCredentials = z
+					.object({
+						email: z.string().email(),
+						password: z.string().min(6),
+					})
+					.safeParse(credentials);
+
+				if (parsedCredentials.success) {
+					const { email, password } = parsedCredentials.data;
+
+					const user = await getUser(email);
+					if (!user) return null;
+
+					const passwordsMatch = await bcrypt.compare(password, user.password);
+
+					if (passwordsMatch) return user;
+				}
+
+				return null;
+			},
+		}),
+	],
+});
